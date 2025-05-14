efad9ee250cf26e505d8a359cbf8200b464f0bea
Victor Daniel <99292901+vdanviel@users.noreply.github.com>
2025-05-14 20:03:54 +0000

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
-})
+import userRouter from './src/router/userRouter.js';
+
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   console.log(`App de exemplo esta rodando na porta ${port}`)
